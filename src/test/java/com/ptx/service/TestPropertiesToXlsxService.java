9a2a7752d0228222323a36de6b1a81e1450e88bb
Alex <alexandru.severin>
2022-08-04 10:51:56 +0300

    Implemented test for generateXlsx

--- src/test/java/com/ptx/service/TestPropertiesToXlsxService.java
+++ src/test/java/com/ptx/service/TestPropertiesToXlsxService.java
@@ -1,11 +1,17 @@
 package com.ptx.service;
 
-import org.apache.commons.io.FileUtils;
+import bad.robot.excel.matchers.WorkbookMatcher;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class TestPropertiesToXlsxService {
@@ -13,15 +19,16 @@ public class TestPropertiesToXlsxService {
 
     public static final String TEST_BUNDLE_FOLDER = "data";
     public static final String TEST_BUNDLE_NAME = "messages";
-    public static final String TEST_OUTPUT_FILE = "out/test-results/messages.xlsx";
+
+    public static final String EXPECTED_RESULT = "data/messages.xlsx"; //predefined file with expected values
 
 
     private final PropertiesToXlsxService propertiesToXlsxService = new PropertiesToXlsxService();
 
     @Test
-    public void generateXlsx() throws IOException {
+    public void generateXlsx() throws IOException, InvalidFormatException {
 
-        File dataFolder = new File(getClass().getClassLoader().getResource(TEST_BUNDLE_FOLDER).getFile());
+        File dataFolder = getFile(TEST_BUNDLE_FOLDER);
         File[] propertyFiles = dataFolder.listFiles((file, name) -> name.startsWith(TEST_BUNDLE_NAME) &&
                 name.endsWith(".properties"));
         if(propertyFiles == null || propertyFiles.length == 0) {
@@ -30,16 +37,20 @@ public class TestPropertiesToXlsxService {
         }
 
         byte[] xlsxFile = propertiesToXlsxService.generateXlsx(TEST_BUNDLE_NAME, propertyFiles);
+        XSSFWorkbook outcome = new XSSFWorkbook(new ByteArrayInputStream(xlsxFile));
 
-        //Write file to disk
-        try(FileOutputStream os = FileUtils.openOutputStream(new File(TEST_OUTPUT_FILE))) {
-            os.write(xlsxFile);
-        }
 
-        //TODO: Assertions
+        File expectedFile = getFile(EXPECTED_RESULT);
+        XSSFWorkbook expectedWorkbook = new XSSFWorkbook(expectedFile);
+
+        assertTrue(WorkbookMatcher.sameWorkbook(expectedWorkbook).matches(outcome));
 
     }
 
+    private File getFile(String testBundleFolder) {
+        URL fileUrl = Objects.requireNonNull(getClass().getClassLoader().getResource(testBundleFolder));
+        return new File(fileUrl.getFile());
+    }
 
 
 }
---
+++ src/test/java/com/ptx/test/util/ComparisonException.java
@@ -0,0 +1,12 @@
+package com.ptx.test.util;
+
+public class ComparisonException extends Exception {
+
+    public ComparisonException(String message) {
+        super(message);
+    }
+
+    public ComparisonException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
