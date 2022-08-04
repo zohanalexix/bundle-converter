9ccc8f35f1f44dbece206cc1767cff3430355409
Alex <alexandru.severin>
2022-08-04 14:47:27 +0300

    Implemented conversion properties->xlsx. Working.

--- src/main/java/com/ptx/service/PropertiesToXlsxService.java
+++ src/main/java/com/ptx/service/PropertiesToXlsxService.java
@@ -1,14 +1,12 @@
 package com.ptx.service;
 
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.commons.collections4.properties.SortedProperties;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.io.*;
+import java.util.*;
 
 public class PropertiesToXlsxService {
 
@@ -16,48 +14,92 @@ public class PropertiesToXlsxService {
 
     public byte[] generateXlsx(String bundleName, File[] propertyFiles) throws IOException {
 
-        //key1=property name, key2=file name
+        //key1=property name, key2=file name, value=cell text
         Map<String, Map<String, String>> propertiesAndValues = new LinkedHashMap<>();
 
         //parse all properties file and populate our Map
+        List<String> fileAliases = new ArrayList<>();
         for (File propertyFile : propertyFiles) {
-            parsePropertyFile(propertiesAndValues, propertyFile);
+            String fileAlias = propertyFile.getName().replace(bundleName, "")
+                    .replace(".properties", "");
+            if(fileAlias.startsWith("_")) {
+                fileAlias = fileAlias.substring(1);
+            }
+
+            fileAliases.add(fileAlias);
+            parsePropertyFile(propertiesAndValues, propertyFile, fileAlias);
         }
 
-
-
-        throw new UnsupportedOperationException();
+        return createXlsx(bundleName, propertiesAndValues, fileAliases);
     }
 
-    private void parsePropertyFile(Map<String, Map<String, String>> propertiesAndValues, File propertyFile) throws IOException {
-        String fileName = propertyFile.getName();
+    private byte[] createXlsx(String sheetName, Map<String, Map<String, String>> propertiesAndValues, List<String> fileAliases) {
 
-        Properties prop = new Properties();
-        prop.load(new FileInputStream(propertyFile));
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet(sheetName);
 
-        prop.forEach((key, value) -> propertiesAndValues
-                .computeIfAbsent(String.valueOf(key), key2 -> new LinkedHashMap<>())
-                .put(fileName, String.valueOf(value)));
-    }
+        XSSFFont fontBold = workbook.createFont();
+        fontBold.setBold(true);
+        XSSFCellStyle styleBold = workbook.createCellStyle();
+        styleBold.setFont(fontBold);
 
-    private byte[] generateXlsx(Map<String, Map<String, String>> propertiesAndValues, String bundleName) throws IOException {
+        // Header row
+        int rowIdx=0;
+        XSSFRow headerRow = sheet.createRow(rowIdx++);
+        for (int i=0; i<fileAliases.size(); i++) {
+            String fileAlias = fileAliases.get(i);
+            XSSFCell headerCell = headerRow.createCell(i + 1);
+            headerCell.setCellValue(fileAlias);
+            headerCell.setCellStyle(styleBold);
+        }
 
-        try(XSSFWorkbook workbook = new XSSFWorkbook()) {
-            XSSFSheet sheet = workbook.createSheet(bundleName);
+        // Values
+        for (Map.Entry<String, Map<String, String>> propertiesRow : propertiesAndValues.entrySet()) {
+            XSSFRow row = sheet.createRow(rowIdx++);
 
-            for (Map.Entry<String, Map<String, String>> propertyAndValues : propertiesAndValues.entrySet()) {
+            String propertyName = propertiesRow.getKey();
+            XSSFCell propertyNameCell = row.createCell(0);
+            propertyNameCell.setCellValue(propertyName);
 
-                String property = propertyAndValues.getKey();
-                Map<String, String> values = propertyAndValues.getValue();
+            Map<String, String> values = propertiesRow.getValue();
+            for (int i=0; i<fileAliases.size(); i++) {
+                String fileAlias = fileAliases.get(i);
+                String textForFile = Objects.requireNonNullElse(values.get(fileAlias), "");
 
-                //TODO: Continue
-
-
+                XSSFCell valueCell = row.createCell(i + 1);
+                valueCell.setCellValue(textForFile);
             }
         }
 
-        throw new UnsupportedOperationException();
+        //add 1 blank row at the end
+        sheet.createRow(rowIdx);
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            workbook.write(outputStream);
+            return outputStream.toByteArray();
+        } catch (IOException e ) {
+            throw new IllegalStateException("Unable to write workbook to output stream", e);
+        }
     }
+
+    private void parsePropertyFile(Map<String, Map<String, String>> propertiesAndValues, File propertyFile, String fileAlias)
+            throws IOException {
+
+        SortedProperties prop = new SortedProperties();
+        prop.load(new FileInputStream(propertyFile));
+
+        Iterator<Object> propIterator = prop.keys().asIterator();
+        while(propIterator.hasNext()) {
+            Object key = propIterator.next();
+            Object value = prop.get(key);
+
+            propertiesAndValues
+                    .computeIfAbsent(String.valueOf(key), key2 -> new LinkedHashMap<>())
+                    .put(fileAlias, String.valueOf(value));
+        }
+
+    }
+
 
 
 }
--- src/test/java/com/ptx/service/TestPropertiesToXlsxService.java
+++ src/test/java/com/ptx/service/TestPropertiesToXlsxService.java
@@ -2,11 +2,15 @@ package com.ptx.service;
 
 import bad.robot.excel.matchers.WorkbookMatcher;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
@@ -18,7 +22,8 @@ public class TestPropertiesToXlsxService {
 
 
     public static final String TEST_BUNDLE_FOLDER = "data";
-    public static final String TEST_BUNDLE_NAME = "messages";
+    public static final String TEST_BUNDLE_NAME   = "messages";
+    public static final String TEST_OUTPUT_FOLDER = "build/test-result/";
 
     public static final String EXPECTED_RESULT = "data/messages.xlsx"; //predefined file with expected values
 
@@ -37,14 +42,30 @@ public class TestPropertiesToXlsxService {
         }
 
         byte[] xlsxFile = propertiesToXlsxService.generateXlsx(TEST_BUNDLE_NAME, propertyFiles);
+        saveFileToDisk(xlsxFile);
+
         XSSFWorkbook outcome = new XSSFWorkbook(new ByteArrayInputStream(xlsxFile));
 
 
         File expectedFile = getFile(EXPECTED_RESULT);
         XSSFWorkbook expectedWorkbook = new XSSFWorkbook(expectedFile);
 
-        assertTrue(WorkbookMatcher.sameWorkbook(expectedWorkbook).matches(outcome));
+        Matcher<Workbook> workbookMatcher = WorkbookMatcher.sameWorkbook(expectedWorkbook);
+        boolean matches = workbookMatcher.matches(outcome);
+        assertTrue(matches);
 
+    }
+
+    private void saveFileToDisk(byte[] xlsxFile) throws IOException {
+        File outputFolder = new File(TEST_OUTPUT_FOLDER);
+        if(!outputFolder.exists()) {
+            if (!outputFolder.mkdirs()) {
+                throw new IllegalStateException("Unable to create output folder");
+            }
+        }
+        try (FileOutputStream outputStream = new FileOutputStream(TEST_OUTPUT_FOLDER + "generateXlsx.xlsx")) {
+            outputStream.write(xlsxFile);
+        }
     }
 
     private File getFile(String testBundleFolder) {
