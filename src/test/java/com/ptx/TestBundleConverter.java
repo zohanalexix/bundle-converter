16dbaba5306e9096fc587cedc04e6d3483cc2509
Alex <alexandru.severin>
2022-08-07 18:43:00 +0300

    Added test for convertXlsxToProperties

--- src/test/java/com/ptx/TestBundleConverter.java
+++ src/test/java/com/ptx/TestBundleConverter.java
@@ -1,18 +1,20 @@
 package com.ptx;
 
 import bad.robot.excel.matchers.WorkbookMatcher;
-import com.ptx.BundleConverter;
+import org.apache.commons.collections4.properties.SortedProperties;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestBundleConverter {
@@ -32,6 +34,7 @@ public class TestBundleConverter {
 
         File outputFolder = new File(TEST_OUTPUT_FOLDER);
         File[] propertyFiles = getTestPropertyFiles();
+        File expectedOutput = getFile(EXPECTED_RESULT);
 
         bundleConverter.convertPropertiesToXlsx(TEST_BUNDLE_NAME, propertyFiles, outputFolder);
 
@@ -40,12 +43,47 @@ public class TestBundleConverter {
 
         XSSFWorkbook outcome = new XSSFWorkbook(createdFile);
 
-        File expectedFile = getFile(EXPECTED_RESULT);
-        XSSFWorkbook expectedWorkbook = new XSSFWorkbook(expectedFile);
+
+        XSSFWorkbook expectedWorkbook = new XSSFWorkbook(expectedOutput);
 
         Matcher<Workbook> workbookMatcher = WorkbookMatcher.sameWorkbook(expectedWorkbook);
         boolean matches = workbookMatcher.matches(outcome);
         assertTrue(matches);
+    }
+
+
+
+    @Test
+    public void convertXlsxToProperties() throws IOException {
+
+        File outputFolder = new File(TEST_OUTPUT_FOLDER);
+        File xlsxFile = getFile(EXPECTED_RESULT);
+        File[] expectedOutput = getTestPropertyFiles();
+
+        bundleConverter.convertXlsxToProperties(xlsxFile, outputFolder);
+
+        String[] generatedFiles = outputFolder.list((dir, name) -> name.endsWith(".properties"));
+
+        assertNotNull(generatedFiles);
+
+        for (File expectedPropertiesFile : expectedOutput) {
+            File outputPropertiesFile = getFile(outputFolder, expectedPropertiesFile.getName());
+
+            SortedProperties expectedProperties = new SortedProperties();
+            expectedProperties.load(new FileInputStream(expectedPropertiesFile));
+
+            SortedProperties actualProperties = new SortedProperties();
+            actualProperties.load(new FileInputStream(outputPropertiesFile));
+
+            assertEquals(expectedProperties.size(), actualProperties.size());
+
+            for (Object expectedKey : expectedProperties.keySet()) {
+                Object expectedValue = expectedProperties.get(expectedKey);
+                Object actualValue = actualProperties.get(expectedKey);
+                assertEquals(expectedValue, actualValue);
+            }
+        }
+
     }
 
 
@@ -60,17 +98,6 @@ public class TestBundleConverter {
         return propertyFiles;
     }
 
-    private void saveFileToDisk(byte[] xlsxFile) throws IOException {
-        File outputFolder = new File(TEST_OUTPUT_FOLDER);
-        if(!outputFolder.exists()) {
-            if (!outputFolder.mkdirs()) {
-                throw new IllegalStateException("Unable to create output folder");
-            }
-        }
-        try (FileOutputStream outputStream = new FileOutputStream(TEST_OUTPUT_FOLDER + "generateXlsx.xlsx")) {
-            outputStream.write(xlsxFile);
-        }
-    }
 
     private File getFile(String testBundleFolder) {
         URL fileUrl = Objects.requireNonNull(getClass().getClassLoader().getResource(testBundleFolder));
@@ -80,7 +107,7 @@ public class TestBundleConverter {
 
     private File getFile(File folder, String fileName) throws IOException {
         File[] files = folder.listFiles((dir, name) -> name.equals(fileName));
-        if(files == null && files.length == 0) {
+        if(files == null || files.length == 0) {
             throw new IOException("No file called '" + fileName + "' found in " + folder.getPath());
         }
         return files[0];
